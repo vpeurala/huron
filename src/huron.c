603064077247a549c90a2538c1f0aab6d4dc0e36
Pekka Enberg <penberg@kernel.org>
2012-05-11 16:03:40 +0300

    VM object data structure and preliminary GC support
    This patch introduces a new VM-internal data structure "struct
    huron_object" that represents Lisp objects suchs as atoms and cons pairs
    and adds preliminary GC support for them.
    
    The GC has a collection phase but as there is no root set detection, all
    objects are reclaimed when "huron_gc_collect()" is called.
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/huron.c
+++ src/huron.c
@@ -1,5 +1,28 @@
-int
-main(void)
+#include "huron/object.h"
+#include "huron/gc.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+static void die(const char *s)
 {
-    return 0;
+	fprintf(stderr, "fatal: %s\n", s);
+	exit(1);
+}
+
+int main(void)
+{
+	int i;
+
+	huron_gc_init();
+
+	for (i = 0; i < 10000; i++) {
+		void *p;
+
+		p = huron_object_new(HURON_OBJECT_ATOM);
+		if (!p)
+			die("out of memory");
+	}
+
+	return 0;
 }
